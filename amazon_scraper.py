cdb8e8b1c58eae038cd24b333d2be35fa13a0847
nikhil-varghese <nikhilvarghese@tutamail.com>
2020-11-17 01:57:53 +0530

    Error handling for the scraper

--- amazon_scraper.py
+++ amazon_scraper.py
@@ -83,71 +83,74 @@ def scraper(product_url):
 	product_title = product_page_parsed.find(id='productTitle').text.strip('\n')
 	st.header(product_title)
 
-	# Average rating for the product
-	average_rating = product_page_parsed.find('i', class_='a-icon-star').text[:3]
-	st.subheader(f"Average Rating:  {average_rating}")
+	try:
+		# Average rating for the product
+		average_rating = product_page_parsed.find('i', class_='a-icon-star').text[:3]
+		st.subheader(f"Average Rating:  {average_rating}")
 
-	num_ratings = product_page_parsed.find(id='acrCustomerReviewText').text[:-8]
-	# num_ratings = int(num_ratings[0].text[:-15])
-	st.subheader(f"No. of ratings: {num_ratings}")
+		num_ratings = product_page_parsed.find(id='acrCustomerReviewText').text[:-7]
+		# num_ratings = int(num_ratings[0].text[:-15])
+		st.subheader(f"No. of ratings: {num_ratings}")
 
-	url_match = product_page_parsed.find("a", class_="a-link-emphasis a-text-bold")["href"]
+		url_match = product_page_parsed.find("a", class_="a-link-emphasis a-text-bold")["href"]
 
-	reviews_url = "https://www.amazon.in" + url_match + "&pageNumber="
-	# print(reviews_url)
+		reviews_url = "https://www.amazon.in" + url_match + "&pageNumber="
+		# print(reviews_url)
 
-	reviews_page = requests.get(reviews_url, headers=headers)
-	# print(reviews_page.status_code)
+		reviews_page = requests.get(reviews_url, headers=headers)
+		# print(reviews_page.status_code)
 
-	reviews_page_parsed = bs(reviews_page.content, 'html.parser')
+		reviews_page_parsed = bs(reviews_page.content, 'html.parser')
 
-	ratings_dist = []
-	for i in reviews_page_parsed.find_all('div', class_='a-meter'):
-	    ratings_dist.append(int(i['aria-label'].strip('%')))
-	s_customer = ratings_dist[0] + ratings_dist[1]
+		ratings_dist = []
+		for i in reviews_page_parsed.find_all('div', class_='a-meter'):
+		    ratings_dist.append(int(i['aria-label'].strip('%')))
+		s_customer = ratings_dist[0] + ratings_dist[1]
 
-	st.subheader(f"4 stars and above: {s_customer}%")
+		st.subheader(f"4 stars and above: {s_customer}%")
 
-	reviews_list = reviews_page_parsed.find(id='cm_cr-review_list')
+		reviews_list = reviews_page_parsed.find(id='cm_cr-review_list')
 
-	# Find total number of reviews to calculate the number of pages to be scraped
-	num_reviews = reviews_page_parsed.find(id='filter-info-section').div.text.replace('\n', '').strip()
-	num_reviews = num_reviews.split('|')[1][1:-15]
-	num_reviews = int(num_reviews.replace(',',''))
-	st.subheader(f"No. of reviews: {num_reviews}")
+		# Find total number of reviews to calculate the number of pages to be scraped
+		num_reviews = reviews_page_parsed.find(id='filter-info-section').div.text.replace('\n', '').strip()
+		num_reviews = num_reviews.split('|')[1][1:-14]
+		num_reviews = int(num_reviews.replace(',',''))
+		st.subheader(f"No. of reviews: {num_reviews}")
 
-	page_limit = math.ceil(num_reviews/10)
-	# print(page_limit)
+		page_limit = math.ceil(num_reviews/10)
+		# print(page_limit)
 
-	all_reviews_df = pd.DataFrame(columns = ['rating', 'title', 'description'])
-	# print(all_reviews_df.head())
-	page = 1
-	latest_iteration = st.empty()
-	bar = st.progress(0)
-	# Call parse function to scrape the reviews
-	while page <= page_limit:
-		full_url = reviews_url + str(page)
-		print(f"Page = {page}/{page_limit}")
-		get_url = requests.get(full_url)
-		print(get_url.status_code)
-
-		while get_url.status_code != 200:
-			time.sleep(2)
-			print("Retrying......")
+		all_reviews_df = pd.DataFrame(columns = ['rating', 'title', 'description'])
+		# print(all_reviews_df.head())
+		page = 1
+		latest_iteration = st.empty()
+		bar = st.progress(0)
+		# Call parse function to scrape the reviews
+		while page <= page_limit:
+			full_url = reviews_url + str(page)
+			print(f"Page = {page}/{page_limit}")
 			get_url = requests.get(full_url)
 			print(get_url.status_code)
 
-		partial_df = review_parse(get_url)
-		progress = round((page/page_limit)*100)
-		all_reviews_df = all_reviews_df.append(partial_df, ignore_index=True)
-		bar.progress(progress)
-		latest_iteration.text(f"Scraping {progress}% completed.")
-		page += 1
-		if page > page_limit:
-			print("Completed.")
+			while get_url.status_code != 200:
+				time.sleep(2)
+				print("Retrying......")
+				get_url = requests.get(full_url)
+				print(get_url.status_code)
 
+			partial_df = review_parse(get_url)
+			progress = round((page/page_limit)*100)
+			all_reviews_df = all_reviews_df.append(partial_df, ignore_index=True)
+			bar.progress(progress)
+			latest_iteration.text(f"Scraping {progress}% completed.")
+			page += 1
+			if page > page_limit:
+				print("Completed.")
+		return all_reviews_df, product_title
 
-	return all_reviews_df, product_title
+	except:
+		st.subheader("No reviews for the product")
+		return None, 1
 
 def product_name(product_title):
 	title = product_title.split('/')
@@ -156,10 +159,10 @@ def product_name(product_title):
 # Save the dataframe of reviews to a csv file
 if st.sidebar.button('Start scraping'):
 	all_reviews_df, product_title = scraper(url)
-	title = product_name(product_title)
-	all_reviews_df.to_csv(f"Reviews/{title} reviews.csv")
-	st.success(f"Saved as '{title} reviews.csv'")
-	holder = 1
+	if product_title != 1:
+		title = product_name(product_title)
+		all_reviews_df.to_csv(f"Reviews/{title} reviews.csv")
+		st.success(f"Saved as '{title} reviews.csv'")
 
 
 def get_wordnet_pos(tag):
@@ -257,7 +260,7 @@ def data_viz(filename):
 	st.pyplot()
 
 	plt.title('Percentage of ratings')
-	plt.pie(x_rating, labels=['Rating 5', 'Rating 4', 'Rating 3', 'Rating 2', 'Rating 1'],
+	plt.pie(x_rating, labels=x_rating.index,
 	        autopct='%1.0f%%', pctdistance=0.7, labeldistance=1.1, wedgeprops=dict(width=0.5))
 	plt.tight_layout()
 	st.pyplot()
